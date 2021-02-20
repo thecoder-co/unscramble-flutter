603e6344de58f88f9a3dd8dd53c58e0965eb5a93
jideguru <guruliciousjide@gmail.com>
2021-02-20 19:49:09 +0100

    (chore:) code cleanup

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,11 +9,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-  );
-
   String _value = '';
 
   void _onSubmit(String value) {
@@ -24,49 +19,16 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() => _value = value);
   }
 
-  List<Widget> _widgetOptions = <Widget>[
-    Container(
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Input Letters',
-                ),
-                autocorrect: true,
-                autofocus: false,
-                keyboardType: TextInputType.text,
-                //onSubmitted: _onSubmit,
-                //onChanged: _onChange,
-              ),
-            ),
-            SizedBox(height: 40),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    child: Text('Unscramble'),
-                    onPressed: () {
-                      print('happy');
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+  List<Widget> _widgetOptions() => <Widget>[
+        _OptionOne(
+          onUnscramble: () {
+            print('happy');
+            // You can do setState now
+            setState(() {});
+          },
         ),
-      ),
-    ),
-    Text(
-      'Index 1: Unscramble',
-      style: optionStyle,
-    ),
-  ];
+        _OptionTwo()
+      ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -93,7 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: _widgetOptions().elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: Colors.green,
@@ -110,6 +72,70 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _OptionOne extends StatelessWidget {
+  final Function onUnscramble;
+
+  _OptionOne({this.onUnscramble});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Input Letters',
+                ),
+                autocorrect: true,
+                autofocus: false,
+                keyboardType: TextInputType.text,
+                //onSubmitted: _onSubmit,
+                //onChanged: _onChange,
+              ),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    child: Text('Unscramble'),
+                    onPressed: () => onUnscramble(),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OptionTwo extends StatelessWidget {
+  final String value;
+
+  _OptionTwo({this.value = ''});
+
+  static const TextStyle optionStyle = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Index 1: Unscrambled $value',
+      style: optionStyle,
     );
   }
 }
