c577e68e63e506da6f41173eea16c8796ea8d14c
thecoder-co <72974286+thecoder-co@users.noreply.github.com>
2021-02-21 21:18:38 +0100

    Add files via upload
    done with basic functionality, I'll complete the looks next weekend

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async' show Future;
+import 'package:flutter/services.dart' show rootBundle;
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -10,6 +12,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
   String _value = '';
+  List _foundWords = [];
+  List _foundWords3 = [];
+  List _foundWords4 = [];
+  List _foundWords5 = [];
+  List _foundWords6 = [];
+  List _foundWords7 = [];
 
   void _onSubmit(String value) {
     setState(() => _value = value);
@@ -19,15 +27,93 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() => _value = value);
   }
 
+  Future<String> loadAsset() async {
+    return await rootBundle.loadString('assets/texts/words_alpha.txt');
+  }
+
   List<Widget> _widgetOptions() => <Widget>[
         _OptionOne(
-          onUnscramble: () {
-            print('happy');
-            // You can do setState now
-            setState(() {});
+          onUnscramble: () async {
+            print(_value);
+            List _toList(String lily) {
+              List a = [];
+              for (var i = 0; i < lily.length; i++) {
+                a.add(lily[i]);
+              }
+
+              return a;
+            }
+
+            int freql(var w, List lst) {
+              int count = 0;
+              for (var i = 0; i < lst.length; i++) {
+                if (lst[i] == w) {
+                  count += 1;
+                }
+              }
+              return count;
+            }
+
+            bool isin(String lily, String ylil) {
+              List a = _toList(lily);
+              List b = _toList(ylil);
+              for (var i = 0; i < a.length; i++) {
+                if (!b.contains(a[i])) {
+                  return false;
+                }
+              }
+              for (var i = 0; i < a.length; i++) {
+                if (freql(a[i], a) > freql(b[i], b)) {
+                  return false;
+                }
+              }
+              return true;
+            }
+
+            List foundWords = [];
+            String fileContent = await loadAsset();
+            List wordList = fileContent.split('\n');
+            List necessaryWords = [];
+
+            for (String i in wordList) {
+              List lst = Iterable<int>.generate(_value.length + 1).toList();
+              if (lst.contains(i.length)) {
+                necessaryWords.add(i);
+              }
+            }
+
+            for (var j in necessaryWords) {
+              if (isin(j, _value)) {
+                foundWords.add(j);
+              }
+            }
+            _foundWords = foundWords;
+            for (var i in foundWords) {
+              if (i.length == 3) {
+                _foundWords3.add(i);
+              } else if (i.length == 4) {
+                _foundWords4.add(i);
+              } else if (i.length == 5) {
+                _foundWords5.add(i);
+              } else if (i.length == 6) {
+                _foundWords6.add(i);
+              } else if (i.length == 7) {
+                _foundWords7.add(i);
+              }
+            }
           },
+          onChange: _onChange,
+          onSubmit: _onSubmit,
         ),
-        _OptionTwo()
+        _OptionTwo(
+          value: _value,
+          foundWords: _foundWords,
+          foundWords3: _foundWords3,
+          foundWords4: _foundWords4,
+          foundWords5: _foundWords5,
+          foundWords6: _foundWords6,
+          foundWords7: _foundWords7,
+        )
       ];
 
   void _onItemTapped(int index) {
@@ -45,7 +131,10 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         elevation: 20,
         centerTitle: true,
-        title: Text('Unscramble'),
+        title: Text(
+          'Unscramble',
+          style: TextStyle(fontSize: 30),
+        ),
         backgroundColor: Colors.green,
         actions: [
           IconButton(
@@ -78,8 +167,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class _OptionOne extends StatelessWidget {
   final Function onUnscramble;
+  final Function onChange;
+  final Function onSubmit;
 
-  _OptionOne({this.onUnscramble});
+  _OptionOne({this.onUnscramble, this.onChange, this.onSubmit});
 
   @override
   Widget build(BuildContext context) {
@@ -91,14 +182,18 @@ class _OptionOne extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(20.0),
               child: TextField(
+                cursorColor: Colors.green,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
                 decoration: InputDecoration(
                   hintText: 'Input Letters',
                 ),
                 autocorrect: true,
                 autofocus: false,
                 keyboardType: TextInputType.text,
-                //onSubmitted: _onSubmit,
-                //onChanged: _onChange,
+                onSubmitted: onSubmit,
+                onChanged: onChange,
               ),
             ),
             SizedBox(height: 40),
@@ -109,7 +204,13 @@ class _OptionOne extends StatelessWidget {
                 children: [
                   ElevatedButton(
                     child: Text('Unscramble'),
-                    onPressed: () => onUnscramble(),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                    onPressed: () {
+                      onUnscramble();
+                    },
                   ),
                 ],
               ),
@@ -123,19 +224,182 @@ class _OptionOne extends StatelessWidget {
 
 class _OptionTwo extends StatelessWidget {
   final String value;
+  final List foundWords;
+  final List foundWords3;
+  final List foundWords4;
+  final List foundWords5;
+  final List foundWords6;
+  final List foundWords7;
 
-  _OptionTwo({this.value = ''});
-
-  static const TextStyle optionStyle = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-  );
+  _OptionTwo(
+      {this.value = '',
+      this.foundWords,
+      this.foundWords3,
+      this.foundWords4,
+      this.foundWords5,
+      this.foundWords6,
+      this.foundWords7});
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'Index 1: Unscrambled $value',
-      style: optionStyle,
+    return Center(
+      child: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: SizedBox(
+              height: 30,
+              child: Text('Three Letter Words'),
+            ),
+          ),
+          SizedBox(
+            height: foundWords3.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords3.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords3[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('Four Letter Words'),
+          ),
+          SizedBox(
+            height: foundWords4.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords4.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords4[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('Five Letter Words'),
+          ),
+          SizedBox(
+            height: foundWords5.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords5.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords5[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('Six Letter Words'),
+          ),
+          SizedBox(
+            height: foundWords6.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords6.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords6[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('Seven Letter Words'),
+          ),
+          SizedBox(
+            height: foundWords7.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords7.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords7[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+            child: Text('All Letter Words'),
+          ),
+          SizedBox(
+            height: foundWords.length == 0 ? 50 : 300,
+            child: Card(
+              color: Colors.green,
+              child: ListView.separated(
+                itemCount: foundWords.length,
+                separatorBuilder: (context, index) => Divider(
+                  color: Colors.white,
+                ),
+                itemBuilder: (context, index) => Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: Text(
+                      foundWords[index],
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
